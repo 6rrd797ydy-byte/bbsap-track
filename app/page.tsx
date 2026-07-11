5f2c1b1a7d8b7a0f98b59119f4dd34263c707e5f
6rrd797ydy-byte <6rrd797ydy@privaterelay.appleid.com>
2026-07-11 09:31:45 +0800

    Update UI Borang Aduan
    Removed duplicate 'use client' directive and added a newline at the end of the file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-
-expo"use client";
+"use client";
 
 import { useState } from "react";
 import { Camera, MapPin, AlertTriangle, Send } from "lucide-react";
@@ -123,3 +122,4 @@ export default function Home() {
       </div>
     </main>
   );
+}
